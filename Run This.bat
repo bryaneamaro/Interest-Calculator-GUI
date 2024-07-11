691cb9d60840458b7e66df70b0aac41241f15f59
Bryan E Amaro <108297472+bryaneamaro@users.noreply.github.com>
2024-07-11 18:15:55 -0400

    Added {source-code/.exe file} and terminal script.
    Dawg Idk If I'm using the right terminology. Oh well- I'll learn.

---
+++ Run This.bat
@@ -0,0 +1,3 @@
+@echo off
+java --module-path "C:\Users\bryan\Downloads\Stinki JFX\javafx-sdk-21.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar C:\Users\bryan\Downloads\GUI_test1.jar
+pause
